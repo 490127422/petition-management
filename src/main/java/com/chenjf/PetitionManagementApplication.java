cbe7dee0dfd22eee072bf45a167e4675a703c8a1
chenjianfeng <jianfeng.chen@powersi.com.cn>
2020-04-02 17:31:42 +0800

    新建信访管理系统项目

---
+++ src/main/java/com/chenjf/PetitionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.chenjf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetitionManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetitionManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chenjf/PetitionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenjf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetitionManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
